7a3276f958a3b61a5c561732ae993caffb67baf3
ZL <ray040406@gmail.com>
2024-12-30 12:00:39 +1300

    mongoose

--- backend/server.js
+++ backend/server.js
@@ -4,3 +4,15 @@ require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("connected to mongodb"))
+  .catch((err) => console.log("mongodb connection error:", err));
+
+app.listen(PORT, () => console.log("server running on port", PORT));
