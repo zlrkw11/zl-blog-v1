a8e585998185f053afeae96c8cfa167a685c6d0e
ZL <ray040406@gmail.com>
2024-12-30 11:50:43 +1300

    frontend

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -1,5 +1,11 @@
 function App() {
-  return <></>;
+  return (
+    <>
+      <div className="flex h-[400px] w-screen border-2 justify-center">
+        post a blog here
+      </div>
+    </>
+  );
 }
 
 export default App;
