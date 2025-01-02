c77fc29bdda55f105a00b812829580cb9d624a0c
ZL <ray040406@gmail.com>
2025-01-02 18:29:17 +1300

    applied cors

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const cors = require("cors");
 const mongoose = require("mongoose");
 require("dotenv").config();
 
+app.use(cors());
 const app = express();
 const PORT = process.env.PORT || 5000;
 
