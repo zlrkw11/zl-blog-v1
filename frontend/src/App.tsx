eede8f25584bc5a716a3e1486868d47a6119d140
ZL <ray040406@gmail.com>
2025-01-02 18:14:33 +1300

    fetch posts function

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,19 @@
+import { useState, useEffect } from "react";
+
 function App() {
+  const [posts, setPosts] = useState([]);
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  const fetchPosts = async () => {
+    const res = await fetch("http://localhost:5000/api/posts");
+    const data = await res.json();
+    setPosts(data);
+  };
+
   return (
     <>
       <div className="flex h-[400px] w-screen border-2 justify-center">
