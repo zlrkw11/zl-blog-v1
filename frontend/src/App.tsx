3fc09934380f0a95887713b678310297483c1b41
ZL <ray040406@gmail.com>
2025-01-02 18:22:19 +1300

    div section

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -14,10 +14,41 @@ function App() {
     setPosts(data);
   };
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const res = await fetch("https://localhost:5000/api/posts", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ title, content }),
+    });
+    // clear out the input boxes
+    if (res.ok) {
+      setTitle("");
+      setContent("");
+      fetchPosts();
+    }
+  };
+
   return (
     <>
       <div className="flex h-[400px] w-screen border-2 justify-center">
-        post a blog here
+        <h1>post a blog here</h1>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            placeholder="Title"
+            required
+          />
+          <textarea
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+            placeholder="Content"
+            required
+          ></textarea>
+          <button type="submit">Add post</button>
+        </form>
       </div>
     </>
   );
