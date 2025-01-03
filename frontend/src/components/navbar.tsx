2be9e5b47d094006dbb5a3b2ac4b74b781d01a82
ZL <ray040406@gmail.com>
2025-01-03 18:53:29 +1300

    css

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -2,8 +2,13 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div className="flex">
-      <Link to="/posts">posts</Link>
+    <div className="px-4 py-2 flex gap-4 border-2 border-blue-200 text-blue-400 ">
+      <Link className="hover:text-blue-600" to="/">
+        home
+      </Link>
+      <Link className="hover:text-blue-600" to="/posts">
+        posts
+      </Link>
     </div>
   );
 };
